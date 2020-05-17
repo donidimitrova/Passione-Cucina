7b8147887834ff9223fb14f25c39b07b351db808
unknown <dimitrovadonika@gmail.com>
2020-05-17 23:39:43 +0200

    cancellato db.php

--- seconda.parte/ajax.php
+++ seconda.parte/ajax.php
@@ -1,6 +1,5 @@
 <?php
-//Including Database configuration file.
-include "db.php";
+
 $con = pg_connect("host=localhost port=5434 dbname=login user=postgres password=12345") or
 die('Could not connect:' . pg_last_error());
 $search=$_POST['tags'];
--- seconda.parte/db.php
+++
@@ -1,4 +0,0 @@
-<?php
-$con = pg_connect("host=localhost port=5434 dbname=login user=postgres password=12345") or
-die('Could not connect:' . pg_last_error());
-?>
