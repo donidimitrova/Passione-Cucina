ffb1f3e46751bced3b1cee101a552e19fac0d5b5
unknown <dimitrovadonika@gmail.com>
2020-05-17 18:09:03 +0200

    contatti

--- mail.php
+++ mail.php
@@ -14,7 +14,7 @@
         if ( empty($name) OR !filter_var($email, FILTER_VALIDATE_EMAIL)  OR empty($subject) OR empty($message)) {
             # Set a 400 (bad request) response code and exit.
             http_response_code(400);
-            echo "Please complete the form and try again.";
+            echo "Completa il form prima del invio!";
             exit;
         }
         
