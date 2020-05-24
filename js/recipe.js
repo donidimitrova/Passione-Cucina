40944e6d47b49ac6884d5f76f13d081f7c069484
unknown <dimitrovadonika@gmail.com>
2020-05-24 23:21:50 +0200

    uova alla contadina

--- js/recipe.js
+++ js/recipe.js
@@ -20,70 +20,48 @@ Vue.component('porzioni', {
   })
 
 
-    window.console = window.console || function(t) {};
- 
 
-    if (document.location.search.match(/type=embed/gi)) {
-      window.parent.postMessage("resize", "*");
-    }
- 
-
-    Vue.component('stelle-rating', {
-    
-      props: {
-        'name': String,
-        'value': null,
-        'id': String,
-        'disabled': Boolean,
-        'required': Boolean },
-    
-    
-      template: '<div class="stelle-rating">\
-            <label class="star-rating__star" v-for="rating in ratings" \
-            :class="{\'is-selected\': ((value >= rating) && value != null), \'is-disabled\': disabled}" \
-            v-on:click="set(rating)" v-on:mouseover="star_over(rating)" v-on:mouseout="star_out">\
-            <input class="star-rating star-rating__checkbox" type="radio" :value="rating" :name="name" \
-            v-model="value" :disabled="disabled">★</label></div>',
-
-      data: function () {
-        return {
-          temp_value: null,
-          ratings: [1, 2, 3, 4, 5] };
-    
+  Vue.component('votazione', { 
+    props: {
+      'name': String,
+      'valore': null,
+      'id': String,
+      'disabled': Boolean,
+      'required': Boolean },
+    template: '#template-stelle',
+    data: function () {
+      return {
+        temp_valore: null,
+        voti: [1, 2, 3, 4, 5] };
+    },
+    methods: {
+      sopra: function (index) {
+        var self = this;
+        if (!this.disabled) {
+          this.temp_valore = this.valore;
+          return this.valore = index;
+        }
+  
       },
-    
-      methods: {
-        star_over: function (index) {
-          var self = this;
-    
-          if (!this.disabled) {
-            this.temp_value = this.value;
-            return this.value = index;
-          }
-    
-        },
-    
-        star_out: function () {
-          var self = this;
-    
-          if (!this.disabled) {
-            return this.value = this.temp_value;
-          }
-        },
-    
-     
-        set: function (value) {
-          var self = this;
-    
-          if (!this.disabled) {
-            this.temp_value = value;
-            return this.value = value;
-          }
-        } } });
-    
-    
-    
-    new Vue({
-      el: '#app' });
+      lascia: function () {
+        var self = this;
+  
+        if (!this.disabled) {
+          return this.valore = this.temp_valore;
+        }
+      },
+      imposta: function (valore) {
+        var self = this;
+  
+        if (!this.disabled) {
+          this.temp_valore = valore;
+          return this.valore = valore;
+        }
+      } } });
+  
+  
+  
+  new Vue({
+    el: '#stelle' });
 
   
