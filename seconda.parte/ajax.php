29a725c4ddcd0abf9ef8eefcb845e451f0ea1e41
aurora.colacchi@outlook.it <aurora.colacchi@outlook.it>
2020-05-27 21:26:24 +0200

    sistema errore nel file ajax

--- seconda.parte/ajax.php
+++ seconda.parte/ajax.php
@@ -4,8 +4,19 @@ $con = pg_connect("host=localhost port=5434 dbname=login user=postgres password=
 die('Could not connect:' . pg_last_error());
 $search=$_POST['tags'];
 $search=strtolower(preg_replace('/\s+/', '', $search));
+if(!$search){
+    echo '<b>Nessun risutato </b>';
+    echo '<br> <b><a href="/seconda.parte/iniziale.html"> Torna alla pagina iniziale </a></b>';
+    exit;
+}
 $q1="select link from Ricettario where id='$search' ";
 $result=pg_query($con,$q1 );
+
 $Result = pg_fetch_object($result);
+if(!$Result){
+    echo '<b>Nessun risutato </b>';
+    echo '<br> <b><a href="/seconda.parte/iniziale.html"> Torna alla pagina iniziale </a></b>';
+    exit;
+}
 header("Location:$Result->link");
 ?>
