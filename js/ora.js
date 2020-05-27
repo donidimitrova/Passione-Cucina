9e111a30910769b70ec03a80325411189579a85e
aurora.colacchi@outlook.it <aurora.colacchi@outlook.it>
2020-05-27 23:27:49 +0200

    messaggio errore ajax

--- js/ora.js
+++ js/ora.js
@@ -3,7 +3,7 @@ var hours = new Date().getHours();
 new Vue({
   el: '#app',
   data: {
-    Mattina: hours < 12,
+    Mattina:hours < 12,
     Pomeriggio: hours >= 12 && hours < 18,
     Sera: hours >= 18
   }
--- seconda.parte/ajax.php
+++ seconda.parte/ajax.php
@@ -5,8 +5,8 @@ die('Could not connect:' . pg_last_error());
 $search=$_POST['tags'];
 $search=strtolower(preg_replace('/\s+/', '', $search));
 if(!$search){
-    echo '<b>Nessun risutato </b>';
-    echo '<br> <b><a href="/seconda.parte/iniziale.html"> Torna alla pagina iniziale </a></b>';
+    echo '<h2 align="center" style="color:red"><b> Ops! Nessuna ricetta inserita</b></h2>
+    <h4  align="center"><a href="/seconda.parte/iniziale.html"  style="text-decoration:none;background-image:"> Clicca qui per tornare alla pagina iniziale </a></h4>';
     exit;
 }
 $q1="select link from Ricettario where id='$search' ";
@@ -14,8 +14,8 @@ $result=pg_query($con,$q1 );
 
 $Result = pg_fetch_object($result);
 if(!$Result){
-    echo '<b>Nessun risutato </b>';
-    echo '<br> <b><a href="/seconda.parte/iniziale.html"> Torna alla pagina iniziale </a></b>';
+    echo '<h2 align="center" style="color:red"><b> Mi dispiace! La ricetta non è presente </b></h2>';
+    echo '<h4  align="center"><a href="/seconda.parte/iniziale.html"  style="text-decoration:none;"> Clicca qui per tornare alla pagina iniziale </a></h4>';
     exit;
 }
 header("Location:$Result->link");
